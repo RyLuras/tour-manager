889d6ab7956dd29ac3ae05bb6fa8293a98eb07e1
Ryan Luras <ryanluras@gmail.com>
2018-10-11 14:15:46 -0700

    weather location middleware test passing

---
+++ test/unit/location.test.js
@@ -0,0 +1,29 @@
+const location = require('../../lib/util/location');
+
+describe('location middleware', () => {
+
+    it('passes weather data from given zip code into req.body and sends to next()', done => {
+
+        const req = { body: { zip: 94061 } };
+    
+        let called = false;
+        
+        const next = () => {
+            called = true;
+
+            const weatherObj = { 
+                city: 'Redwood City', 
+                state: 'CA', 
+                zip: '94061' 
+            };
+
+            expect(req.stop.location).toEqual(weatherObj);
+            expect(req.stop.weather.temperature).toEqual(expect.any(String));
+            expect(req.stop.weather.condition).toEqual(expect.any(String));
+            expect(called).toBeTruthy();
+            done();
+        };
+    
+        location(req, null, next); 
+    });
+});
