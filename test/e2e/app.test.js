858bce963d7153e1691964efc98790dcc2b1cacb
Ryan Luras <ryanluras@gmail.com>
2018-10-16 16:55:43 -0700

    setting up from alst week, starting e2e app test

---
+++ test/e2e/app.test.js
@@ -0,0 +1,17 @@
+const { dropCollection } = require('./db');
+const request = require('supertest');
+const app = require('../../lib/app');
+
+
+describe('validates Tour routes with Stops generated with Weather Middleware', () => {
+
+    it('Posts to Studio', () => {
+        
+    });
+
+
+
+
+
+});
+
