49015de743354db2364d6ae57cb1b799fbc15d46
Ryan Luras <ryanluras@gmail.com>
2018-10-17 10:34:57 -0700

    tour get test passing

--- test/e2e/app.test.js
+++ test/e2e/app.test.js
@@ -5,7 +5,47 @@ const app = require('../../lib/app');
 
 describe('validates Tour routes with Stops generated with Weather Middleware', () => {
 
-    it('Posts to Studio', () => {
+    let tours = [
+        {
+            title: 'West Coast Tour',
+            activities: ['Rockin', 'rollin', 'playin the blues'],
+            launchDate: Date.now()
+        },
+        {
+            title: 'East Coast Tour',
+            activities: ['Rockin', 'rollin', 'playin the blues'],
+            launchDate: Date.now()
+
+        },
+        {
+            title: 'Great Lakes Tour',
+            activities: ['Rockin', 'rollin', 'playin the blues'],
+            launchDate: Date.now()
+
+        }
+    ];
+    
+    let createdTours;
+
+    const createTour = tour => {
+        return request(app)
+            .post('/api/tours')
+            .send(tour)
+            .then(res => res.body);
+    };
+
+    beforeEach(() => {
+        return dropCollection('tours');
+    });
+
+    beforeEach(() => {
+        return Promise.all(tours.map(createTour)).then(toursRes => {
+            createdTours = toursRes;
+        });
+    });
+
+
+    it('Posts to Tour', () => {
         return request(app)
             .post('/api/tours')
             .send({
@@ -18,5 +58,13 @@ describe('validates Tour routes with Stops generated with Weather Middleware', (
             });
     });
 
-
+    it('gets all Tours', () => {
+        return request(app)
+            .get('/api/tours')
+            .then(res => {
+                expect(res.body).toEqual(createdTours);
+            });
+    });
+ 
 });
+
